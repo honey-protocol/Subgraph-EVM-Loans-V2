22b4ca130597dc1e10bdb89bb76d9b32ae086139
BowTiedFirefox <BowTiedFirefox@protonmail.com>
2023-01-24 21:44:38 -0500

    add underlying events

--- generated/schema.ts
+++ generated/schema.ts
@@ -218,3 +218,62 @@ export class Collateral extends Entity {
     this.set("active", Value.fromBoolean(value));
   }
 }
+
+export class UserUnderlying extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save UserUnderlying entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type UserUnderlying must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("UserUnderlying", id.toString(), this);
+    }
+  }
+
+  static load(id: string): UserUnderlying | null {
+    return changetype<UserUnderlying | null>(store.get("UserUnderlying", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get hTokenAddr(): string {
+    let value = this.get("hTokenAddr");
+    return value!.toString();
+  }
+
+  set hTokenAddr(value: string) {
+    this.set("hTokenAddr", Value.fromString(value));
+  }
+
+  get userAddr(): string {
+    let value = this.get("userAddr");
+    return value!.toString();
+  }
+
+  set userAddr(value: string) {
+    this.set("userAddr", Value.fromString(value));
+  }
+
+  get amount(): BigInt {
+    let value = this.get("amount");
+    return value!.toBigInt();
+  }
+
+  set amount(value: BigInt) {
+    this.set("amount", Value.fromBigInt(value));
+  }
+}
